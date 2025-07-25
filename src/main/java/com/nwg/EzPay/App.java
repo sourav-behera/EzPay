793cbdadb6b8fc67584f8d86f4b018de8d1ecbc7
sourav-behera <souravdipu2002@gmail.com>
2025-07-25 18:23:50 +0530

    Init

---
+++ src/main/java/com/nwg/EzPay/App.java
@@ -0,0 +1,13 @@
+package com.nwg.EzPay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
